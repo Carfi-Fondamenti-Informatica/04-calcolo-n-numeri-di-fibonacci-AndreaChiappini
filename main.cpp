7c9243b2a50db7422f4f5bc1fed0b12b7baf7b36
AndreaChiappini <117998815+AndreaChiappini@users.noreply.github.com>
2022-11-23 18:04:31 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   return 0;
+    int main() {
+int n=0,x=1,y=1,z=0;
+cin >> n;
+        if (n>=2){
+            cout << x << endl;
+            cout << y << endl;
+            for (int i=0;i<n-1;i++){
+                int temp = x;
+                y = temp;
+                x += y;
+                cout << y << endl;
+            }
+
+
+    }else {
+        cout << "errore" << endl;
 }
+        return 0;
+    }
